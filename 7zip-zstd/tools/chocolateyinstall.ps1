9f1057b805cbad948cdaa8f81dd141803fb1adae
Ilya Zverev <izverev@logrocon.com>
2020-08-12 00:39:41 +0300

    result installation script

--- 7zip-zstd/tools/chocolateyinstall.ps1
+++ 7zip-zstd/tools/chocolateyinstall.ps1
@@ -11,11 +11,14 @@ else { Split-Path $packageArgs["url"] -Leaf }
 $packageArgs["packageName"] = "$($env:ChocolateyPackageName)"
 $packageArgs["fileFullPath"] = "$(Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) $filename)"
 
-$ArchivePath = Get-ChocolateyWebFile @packageArgs 
+$archiveLocation = Get-ChocolateyWebFile @packageArgs 
+$spliter = "path to executable:"
+$7zLocation = "$(Split-Path -parent ((7z --shimgen-noop | Select-String $spliter) -split $spliter | ForEach-Object Trim)[1])"
+$installLocation = "$(Join-Path $7zLocation "Codecs")"
 
+Write-Host "Extract files to $installLocation" -ForegroundColor Blue
 if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
-       #TODO: x64
-        }
+       7z e "$($archiveLocation)"  -o"$($installLocation)"  *-x64.dll -y -r }
 else {
-       #TODO: x86 
-       }
+       7z e "$($archiveLocation)"  -o"$($installLocation)"  *-x32.dll -y -r }
+Write-Host "Extraction completed" -ForegroundColor Blue
