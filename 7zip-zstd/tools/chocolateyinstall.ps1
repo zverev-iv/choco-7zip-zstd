ba799f4cb02de557cce5d2ae997b087aa5a0dba6
Ilya Zverev <izverev@logrocon.com>
2020-08-11 23:43:47 +0300

    final form?

--- 7zip-zstd/tools/chocolateyinstall.ps1
+++ 7zip-zstd/tools/chocolateyinstall.ps1
@@ -4,20 +4,18 @@ $meta = Get-Content -Path $env:ChocolateyPackageFolder\tools\packageArgs.json -R
 $packageArgs = @{}
 (ConvertFrom-Json $meta).psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
 
-$filename = ""
-$filePath = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
-       $filename = Split-Path $packageArgs["url64bit"] -Leaf}
-else { $filename = Split-Path $packageArgs["url"] -Leaf }
-
-if(!$filename)
-{
-  Write-ChocolateyFailure $packageArgs["packageName"] "filename not found in package url"
-}
+$filename = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
+       Split-Path $packageArgs["url64bit"] -Leaf }
+else { Split-Path $packageArgs["url"] -Leaf }
 
 $packageArgs["packageName"] = "$($env:ChocolateyPackageName)"
 $packageArgs["fileFullPath"] = "$(Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) $filename)"
 
-Get-ChocolateyWebFile @packageArgs 
-#TODO: unpack to working directory
+$ArchivePath = Get-ChocolateyWebFile @packageArgs 
 
-
+if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
+       #TODO: x64
+        }
+else {
+       #TODO: x86 
+       }
