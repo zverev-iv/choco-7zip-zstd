3be8565b6b745051cf10cfa0d581639a367740e2
Ilya Zverev <izverev@logrocon.com>
2020-08-12 11:52:39 +0300

    add uninstall

--- 7zip-zstd/tools/chocolateyinstall.ps1
+++ 7zip-zstd/tools/chocolateyinstall.ps1
@@ -17,7 +17,7 @@ $spliter = "path to executable:"
 $7zLocation = "$(Split-Path -parent ((7z --shimgen-noop | Select-String $spliter) -split $spliter | ForEach-Object Trim)[1])"
 $installLocation = "$(Join-Path $7zLocation "Codecs")"
 
-Write-Host "Extract files" -ForegroundColor Blue
+Write-Host "Install libraries" -ForegroundColor Blue
 if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
        7z e "$($archiveLocation)" -o"$($extractLocation)"  *-x64.dll -y -r }
 else {
@@ -25,4 +25,4 @@ else {
  
 ForEach($library in $packageArgs["libraries"]) {
        Copy-Item "$(Join-Path $extractLocation ($library+"*"))" "$($installLocation)" -Force }
-Write-Host "Extraction completed" -ForegroundColor Blue
+Write-Host "Install completed" -ForegroundColor Blue
