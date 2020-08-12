46c43f917c14f43a450911ee4e8ca7b888be67a8
Ilya Zverev <izverev@logrocon.com>
2020-08-12 11:43:35 +0300

    fix installation

--- 7zip-zstd/tools/chocolateyinstall.ps1
+++ 7zip-zstd/tools/chocolateyinstall.ps1
@@ -15,11 +15,14 @@ $archiveLocation = Get-ChocolateyWebFile @packageArgs
 $extractLocation = "$(Join-Path (Split-Path -parent $archiveLocation) "Codecs")"
 $spliter = "path to executable:"
 $7zLocation = "$(Split-Path -parent ((7z --shimgen-noop | Select-String $spliter) -split $spliter | ForEach-Object Trim)[1])"
+$installLocation = "$(Join-Path $7zLocation "Codecs")"
 
 Write-Host "Extract files" -ForegroundColor Blue
 if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
-       7z e "$($archiveLocation)"  -o"$($extractLocation)"  *-x64.dll -y -r }
+       7z e "$($archiveLocation)" -o"$($extractLocation)"  *-x64.dll -y -r }
 else {
-       7z e "$($archiveLocation)"  -o"$($extractLocation)"  *-x32.dll -y -r }
-Copy-Item "$($extractLocation)" "$($7zLocation)" -Force -Recurse -Verbose
+       7z e "$($archiveLocation)" -o"$($extractLocation)"  *-x32.dll -y -r }
+ 
+ForEach($library in $packageArgs["libraries"]) {
+       Copy-Item "$(Join-Path $extractLocation ($library+"*"))" "$($installLocation)" -Force }
 Write-Host "Extraction completed" -ForegroundColor Blue
