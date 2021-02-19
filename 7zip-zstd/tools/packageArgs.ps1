a953b9b9610b6321c112f17493320274a6bd08dc
zverev iv <mail@zverev-iv.me>
2021-02-19 19:04:14 +0300

    change  to pson + Invoke-Expresion

---
+++ 7zip-zstd/tools/packageArgs.ps1
@@ -0,0 +1,8 @@
+@{
+    url            = "https://github.com/mcmilk/7-Zip-zstd/releases/download/19.00-v1.4.5-R4/Codecs.7z";
+    checksum       = "71831800A8A6A7361300D3AD4CB1C42B235BAECC2F7C0424C2F5226629DD5CA2";
+    checksumType   = "sha256";
+    url64bit       = "https://github.com/mcmilk/7-Zip-zstd/releases/download/19.00-v1.4.5-R4/Codecs.7z";
+    checksum64     = "71831800A8A6A7361300D3AD4CB1C42B235BAECC2F7C0424C2F5226629DD5CA2";
+    checksumType64 = "sha256";
+}
