22475ddad140640777d770da10716019ad1bf569
zverev-iv <51955637+zverev-iv@users.noreply.github.com>
2021-11-08 19:28:17 +0300

    Update packageArgs.ps1

--- 7zip-zstd/tools/packageArgs.ps1
+++ 7zip-zstd/tools/packageArgs.ps1
@@ -1,8 +1,8 @@
 @{
-    url            = "https://github.com/mcmilk/7-Zip-zstd/releases/download/19.00-v1.4.5-R4/Codecs.7z";
-    checksum       = "71831800A8A6A7361300D3AD4CB1C42B235BAECC2F7C0424C2F5226629DD5CA2";
+    url            = "https://github.com/mcmilk/7-Zip-zstd/releases/download/19.00-v1.4.9-R2/Codecs-x32.7z";
+    checksum       = "C91F1AF0BC0C89F85484A3B90C537538FA3AC45306962BF7483E3E36605584FD";
     checksumType   = "sha256";
-    url64bit       = "https://github.com/mcmilk/7-Zip-zstd/releases/download/19.00-v1.4.5-R4/Codecs.7z";
-    checksum64     = "71831800A8A6A7361300D3AD4CB1C42B235BAECC2F7C0424C2F5226629DD5CA2";
+    url64bit       = "https://github.com/mcmilk/7-Zip-zstd/releases/download/19.00-v1.4.9-R2/Codecs-x64.7z";
+    checksum64     = "9D91655E39F5D1F186792C66641E71AE4DBE9CF7BC4C73B3FA784026A66E2653";
     checksumType64 = "sha256";
 }
